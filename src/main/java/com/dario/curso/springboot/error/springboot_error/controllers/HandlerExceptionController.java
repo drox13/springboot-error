7c7ad8571e5cbbefed0713a2b03ccc7011443a27
drox13 <dario.baron@uptc.edu.co>
2024-12-04 09:59:48 -0500

    83. Personalizando página de error para la excepción NumberFormatException

--- src/main/java/com/dario/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/dario/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -1,12 +1,15 @@
 package com.dario.curso.springboot.error.springboot_error.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.text.html.parser.Entity;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -34,5 +37,17 @@ public class HandlerExceptionController {
         error.setMessage(e.getMessage());
         error.setStatus(HttpStatus.NOT_FOUND.value());
         return ResponseEntity.status(404).body(error);
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormatException(Exception e){
+        Map<String, Object> error = new HashMap<>();
+        error.put("date", new Date());
+        error.put("error", "numero invalido o incorrecto , no tiene el formato de digito");
+        error.put("message", e.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value()+"");
+
+        return error;
     }
 }
