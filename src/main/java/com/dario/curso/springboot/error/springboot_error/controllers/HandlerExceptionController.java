a5619ebfb5e6e4f4508be3b040e8df772dc15974
drox13 <dario.baron@uptc.edu.co>
2024-12-04 09:31:58 -0500

    82. Personalizando error 404

--- src/main/java/com/dario/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/dario/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -2,10 +2,13 @@ package com.dario.curso.springboot.error.springboot_error.controllers;
 
 import java.util.Date;
 
+import javax.swing.text.html.parser.Entity;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.dario.curso.springboot.error.springboot_error.models.Error;
 
@@ -21,5 +24,15 @@ public class HandlerExceptionController {
         error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         //return ResponseEntity.internalServerError().body(error);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFoundEx(NoHandlerFoundException e){
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Api Rest no encontrado");
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(404).body(error);
     }
 }
